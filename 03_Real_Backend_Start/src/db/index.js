de6473147efa51b0a47b427829324f996ffc9a75
deepak <deepakdas538@gmail.com>
2024-07-31 16:50:13 +0530

    connect mongodb database

---
+++ 03_Real_Backend_Start/src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose, { mongo } from "mongoose"
+import {DB_NAME} from "../constant.js"
+
+
+
+const connectDB=async ()=>{
+    try {
+       const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       console.log(`mongodb connected || DB_HOST : ${connectionInstance.connection.host}`);
+        
+    } catch (error) {
+        console.log("MONGODB ERROR:",error);
+        process.exit(1)
+        
+    }
+}
+export default connectDB;
