bf5232e98bcb46eb44873529c4c80fdb7abf5c2c
deepak <deepakdas538@gmail.com>
2024-08-08 16:17:29 +0530

    ADD use postman for backend

--- 03_Real_Backend_Start/src/controllers/user.controller.js
+++ 03_Real_Backend_Start/src/controllers/user.controller.js
@@ -20,7 +20,7 @@ const registerUser=asyncHandler(async(req,res)=>{
     //-------------------------------------------
 
     const {fullname,email,username,password}=req.body;
-    console.log("email:",email);
+    // console.log("email:",email);
     
     if(fullname===""){
         throw new ApiError(400,"fullname is required")
@@ -35,7 +35,7 @@ const registerUser=asyncHandler(async(req,res)=>{
         throw new ApiError(400,"password is requried")
     }
 
-    const existedUser =  User.findOne({
+    const existedUser = await User.findOne({
         $or:[{username},{email}]
     })
 
@@ -44,13 +44,17 @@ const registerUser=asyncHandler(async(req,res)=>{
     }
 
     const avatarLocalPath=req.files?.avatar[0]?.path
-    const coverImageLocalPath=req.files?.coverImage[0].path
+    // const coverImageLocalPath=req.files?.coverImage[0].path
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) &&req.files.coverImage.length>0){
+        coverImageLocalPath=req.files.coverImage[0].path
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400,"avatar file is required")
     }
     const avatar=await uploadOnCloudinary(avatarLocalPath);
-    const cover=await uploadOnCloudinary(coverImageLocalPath);
+    const coverImage=await uploadOnCloudinary(coverImageLocalPath);
     if(!avatar){
         throw new ApiError(400,"avatar file is required")
     }
@@ -66,7 +70,7 @@ const registerUser=asyncHandler(async(req,res)=>{
    const createdUser=await User.findById(user._id).select(
     "-password -refreshToken"
    )
-   if(createdUser){
+   if(!createdUser){
     throw new ApiError(500,"something went wrong while registering the users")
    }
 
